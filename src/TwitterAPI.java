a2463d05b65641db79fe072a7a10abd1e5ace8b6
Rafael Gaspar <rafaelgaspar@hotmail.com.br>
2016-10-07 12:19:10 -0300

    Started Twitter API integration

---
+++ src/TwitterAPI.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterAPI {
+	private static final String TWITTER_CONSUMER_KEY = "di1tNQeO3Evfr0WVtM6G7rhLk";
+	private static final String TWITTER_SECRET_KEY = "yBdmCqtEvJTjGvv5oxXDqAoL3Me8LazVuDTqao3sI5aQHhgnGV";
+	private static final String TWITTER_ACCESS_TOKEN = "193012190-gtmI9EepYjEhNL7y6mpjHfm0g4XfdBetUnt69jW7";
+	private static final String TWITTER_ACCESS_TOKEN_SECRET = "NS8CILBytfUXrNpFoRbnaPhxvP87PoDlBculDDZ3kpNsC";
+
+	public static void main(String[] args){
+		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		    .setOAuthConsumerKey(TWITTER_CONSUMER_KEY)
+		    .setOAuthConsumerSecret(TWITTER_SECRET_KEY)
+		    .setOAuthAccessToken(TWITTER_ACCESS_TOKEN)
+		    .setOAuthAccessTokenSecret(TWITTER_ACCESS_TOKEN_SECRET);
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		try {
+		    Query query = new Query("Grande Família");
+		    QueryResult result;
+		    do {
+		        result = twitter.search(query);
+		        List<Status> tweets = result.getTweets();
+		        for (Status tweet : tweets) {
+		            System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+		        }
+		    } while ((query = result.nextQuery()) != null);
+		    System.exit(0);
+		} catch (TwitterException te) {
+		    te.printStackTrace();
+		    System.out.println("Failed to search tweets: " + te.getMessage());
+		    System.exit(-1);
+		}
+	}
+}
