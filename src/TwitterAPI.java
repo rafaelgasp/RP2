ead594174bfe5280b47a06e95a691e3deae1ec89
Rafael Gaspar <rafaelgaspar@hotmail.com.br>
2016-10-11 16:04:06 -0300

    Yandex Translate API

--- src/TwitterAPI.java
+++ src/TwitterAPI.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -8,6 +6,11 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.List;
+
+import com.rmtheis.yandtran.language.Language;
+import com.rmtheis.yandtran.translate.Translate;
+
 public class TwitterAPI {
 	private static final String TWITTER_CONSUMER_KEY = "di1tNQeO3Evfr0WVtM6G7rhLk";
 	private static final String TWITTER_SECRET_KEY = "yBdmCqtEvJTjGvv5oxXDqAoL3Me8LazVuDTqao3sI5aQHhgnGV";
@@ -15,6 +18,17 @@ public class TwitterAPI {
 	private static final String TWITTER_ACCESS_TOKEN_SECRET = "NS8CILBytfUXrNpFoRbnaPhxvP87PoDlBculDDZ3kpNsC";
 
 	public static void main(String[] args){
+		Translate.setKey("trnsl.1.1.20161011T185140Z.c71d1c17ed00525a.d081564173bc99c47fb6b5211567c5ccd26160bc");
+
+        String translatedText = null;
+		try {
+			translatedText = Translate.execute("Hola, mundo!", Language.SPANISH, Language.PORTUGUESE);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        System.out.println(translatedText);
 		
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true)
@@ -25,13 +39,13 @@ public class TwitterAPI {
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		Twitter twitter = tf.getInstance();
 		try {
-		    Query query = new Query("Grande Família");
+		    Query query = new Query("Palmeiras");
 		    QueryResult result;
 		    do {
 		        result = twitter.search(query);
 		        List<Status> tweets = result.getTweets();
 		        for (Status tweet : tweets) {
-		            System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+		            //System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
 		        }
 		    } while ((query = result.nextQuery()) != null);
 		    System.exit(0);
---
+++ src/com/rmtheis/yandtran/ApiKeys.java
@@ -0,0 +1,5 @@
+package com.rmtheis.yandtran;
+
+public class ApiKeys {
+  public static final String YANDEX_API_KEY = "trnsl.1.1.20161011T185140Z.c71d1c17ed00525a.d081564173bc99c47fb6b5211567c5ccd26160bc";
+}
