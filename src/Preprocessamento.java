5b6e417826440867dda04d0125f6a258f62363c5
alexgjlan <alexlan46@gmail.com>
2016-11-17 02:01:56 -0200

    Revert "Preprocessamento final"
    This reverts commit c430875202458cb3c3c6dbc5425a9acf106542e6.

--- src/Preprocessamento.java
+++ src/Preprocessamento.java
@@ -4,17 +4,20 @@ import java.util.regex.Pattern;
 
 class Preprocessamento{
 
-        public static String preprocessar(String line){
-            String[] linesplit = line.split(" ");
-            String resp = "";
-            for(int i = 0 ; i < linesplit.length ; i++){
-                resp = resp + extrairInfo(linesplit[i]);
-            }
-            return resp;
-        } 
-                
+	/*public static void main (String args[]){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite algo que o fara se arrepender depois:");
+		while(sc.hasNext()){
+			String line = sc.nextLine();
+			extrairInfo(line);
+		}
+
+	}*/
+
 	public static String extrairInfo(String line){
-                String resp = "";
+		line = line.replace("\n", " ").replace("\r", " ");
+
 		String usertag = "((\\s)?(@[\\w]+))";
 		String hashtag = "((\\s)?(#[\\w]+))";
 		String url = "((\\s)?((http(s)?)|(ftp)://)?([\\d\\w-/]+)(\\.)([a-zA-Z]{2,6})([^\\s]*)(/)?)";
@@ -35,30 +38,12 @@ class Preprocessamento{
 		l.add(url);
 		l.add(regexPos); l.add(regexPosInv);
 		l.add(regexNeg); l.add(regexNegInv);
-		
-		String temp = "";		
 
-		for(int i = 0 ; i < l.size(); i++){
-
-			// Create a Pattern object
-			Pattern r = Pattern.compile(l.get(i));
-
-			// Now create matcher object.
-			Matcher m = r.matcher(line);
-		
-			if(m.find()){
-				
-				//substituir por leitura de arquivo
-				if(i==0) temp= line.replace(line,"\\USERTAG");
-				else if(i==1) temp = line.replace(line,"\\HASHTAG");
-				else if(i==2) temp = line.replace(line,"\\URL");
-                                else if(i==3 || i==4) temp = line.replace(line,"happy");
-                                else temp = line.replace(line,"sad");
-				break;
-			}
-			else temp = line;
-			
+		for(int i = 0 ; i < l.size(); i++){		
+			line = line.replaceAll(l.get(i), " ");
 		}
-		return temp + " ";
+		
+		return (line);
 	}
 }
+
