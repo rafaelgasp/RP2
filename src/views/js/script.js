ed2c021c2eb177d0ef64b880062fd75d5faa66e3
fabioconejo <fabio.conejo@hotmail.com>
2016-12-09 19:35:07 -0200

    ultima atualização
    é tetraaaaaaa

--- src/views/js/script.js
+++ src/views/js/script.js
@@ -1,5 +1,25 @@
+var programa;
+
 function go(loc) {
-	if (document.getElementById('telao').src.indexOf(loc) == -1) {
+	window.programa = document.getElementById("lista-programa").value;
+	var opcao = document.getElementById('telao').src.indexOf(loc);
+	
+	if (programa != "Selecione..." && opcao == -1) {
 		document.getElementById('telao').src = loc;
 	}
+}
+
+function proximaEtapa() {
+	window.programa = document.getElementById("lista-programa").value;
+	
+	if(programa != "Selecione...") {
+		go("relatorios/escolha_acao.html");
+	}
+	else {
+		go("relatorios/escolha_programa.html");
+	}
+}
+
+function recebePrograma() {
+	document.getElementById('graf').style.backgroundImage = "url('../imagens/graf-The Voice Brasil.png')";
 }
